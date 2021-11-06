28c1f202d85ed112460d012502c1d2e2c10f278a
Daiya Mita <daiyamaxus@gmail.com>
2021-11-07 00:48:57 +0900

    プロジェクト ファイルを追加します。

---
+++ Class3/SimpleMC.cpp
@@ -0,0 +1,53 @@
+
+#include "SimpleMC.h"
+#include "UTRandom1.h"
+#include <cmath>
+
+
+using namespace std;
+
+double SimpleMonteCarlo2(const PayOff& thePayOff,
+                         double Expiry,  
+						 double Spot, 
+						 double Vol, 
+						 double r, 
+						 unsigned long NumberOfPaths)
+{
+
+	double variance = Vol*Vol*Expiry;
+	double rootVariance = sqrt(variance);
+	double itoCorrection = -0.5*variance;
+
+	double movedSpot = Spot*exp(r*Expiry +itoCorrection);
+	double thisSpot;
+	double runningSum=0;
+
+	for (unsigned long i=0; i < NumberOfPaths; i++)
+	{
+		double thisGaussian = GetOneGaussianByBoxMuller();
+		thisSpot = movedSpot*exp( rootVariance*thisGaussian);
+		double thisPayOff = thePayOff(thisSpot);
+		runningSum += thisPayOff;
+	}
+
+	double mean = runningSum / NumberOfPaths;
+	mean *= exp(-r*Expiry);
+	return mean;
+}
+
+/*
+ *
+ * Copyright (c) 2002
+ * Mark Joshi
+ *
+ * Permission to use, copy, modify, distribute and sell this
+ * software for any purpose is hereby
+ * granted without fee, provided that the above copyright notice
+ * appear in all copies and that both that copyright notice and
+ * this permission notice appear in supporting documentation.
+ * Mark Joshi makes no representations about the
+ * suitability of this software for any purpose. It is provided
+ * "as is" without express or implied warranty.
+ * 
+ *  Diva Analytics modified this file on Feb 2, 2016.
+*/
