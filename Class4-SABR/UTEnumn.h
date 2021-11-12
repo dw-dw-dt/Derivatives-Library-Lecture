0c6f3da48636cdee92c009f3f3d8adb66c5e30b8
Daiya Mita <daiyamaxus@gmail.com>
2021-11-12 15:18:54 +0900

    class4

---
+++ Class4-SABR/UTEnumn.h
@@ -0,0 +1,36 @@
+/* UTEnumn.h
+*
+* Copyright (c) 2016
+* Diva Analytics
+*/
+
+#ifndef UT_ENUM_H
+#define UT_ENUM_H
+
+#include <iostream>
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+enum UT_PayReceive
+{
+	UT_INVALID_PAY_RECEIVE = 0,
+	UT_PAY = -1,
+	UT_RECEIVE = 1
+};
+
+///////////////////////////////////////////////////////////////////////////////
+enum UT_CallPut
+{
+	UT_INVALID_CALL_PUT = 0,
+	UT_CALL = 1,
+	UT_PUT = -1,
+	UT_STRADDLE = 2
+};
+
+//Helper functions
+UT_CallPut toCallPut(const std::string &strIn);
+std::string toString(UT_CallPut type);
+
+///////////////////////////////////////////////////////////////////////////////
+
+#endif // UT_ENUM_H
